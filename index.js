30247e93241fff14f3dee25e4e4f0850d907161b
Rafael Willians <me@rwillians.com>
2017-07-01 01:21:51 -0300

    Improved naming and readability

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 
 module.exports = function rescue (callback) {
   return async (...args) => {
-    const next = args.slice(-1).pop()
-    try { return await callback.call(args) } catch (err) { return next(err) }
+    const handler = args.slice(-1).pop()
+
+    try {
+      return await callback.call(args)
+    } catch (err) {
+      return handler(err)
+    }
   }
 }
